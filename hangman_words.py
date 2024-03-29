5d0312039c9fe0740a3697ea41e1bb2e5ace8977
Tharun Karthick T <119483447+Tharunmoonwalker@users.noreply.github.com>
2024-03-29 15:25:26 +0530

    Create hangman_words.py

---
+++ hangman_words.py
@@ -0,0 +1,14 @@
+word_list = [
+    "apple", "banana", "carrot", "dog", "cat", "mouse", "house", "tree",
+    "flower", "sun", "moon", "star", "book", "pen", "pencil", "desk", "chair",
+    "table", "lamp", "door", "window", "bird", "fish", "frog", "snake",
+    "rabbit", "turtle", "lion", "elephant", "giraffe", "zebra", "monkey",
+    "horse", "cow", "sheep", "pig", "duck", "chicken", "goat", "tiger", "bear",
+    "wolf", "fox", "camel", "rhino", "hippo", "penguin", "seal", "whale",
+    "shark", "dolphin", "octopus", "spider", "ant", "bee", "butterfly",
+    "dragonfly", "mosquito", "grasshopper", "ladybug", "cockroach", "scorpion",
+    "beetle", "snail", "worm", "ostrich", "eagle", "hawk", "vulture", "parrot",
+    "crow", "pelican", "swan", "stork", "owl", "woodpecker", "robin",
+    "bluejay", "sparrow", "canary", "finch", "goldfish", "koi", "guppy",
+    "swordtail", "platy", "molly", "bettа", "tеtrа", "angelfish", "barb"
+]
